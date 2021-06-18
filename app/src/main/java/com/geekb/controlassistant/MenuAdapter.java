b7b4a44eb534f431a1b962bfb7d486cee0122124
xiao <xiao@uxin.com>
2021-06-18 12:46:18 +0800

    蓝牙控制添加网络请求

--- app/src/main/java/com/geekb/controlassistant/MenuAdapter.java
+++ app/src/main/java/com/geekb/controlassistant/MenuAdapter.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SwitchCompat;
@@ -15,8 +16,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuHolder> {
-    private String[] mControls = {"自定义手势1", "自定义手势2", "自定义手势3", "自定义手势4", "自定义手势5"};
+    private String[] mControls = {"自定义手势1", "自定义手势2", "自定义手势3", "自定义手势4", "自定义手势5",
+            "自定义手势6", "自定义手势7", "自定义手势8"};
     private Context mContext;
     private List<BindData> mList;
     private ItemClickListener mItemClickListener;
@@ -45,17 +51,40 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuHolder> {
     @Override
     public void onBindViewHolder(@NonNull MenuHolder holder, int position) {
         BindData data = mList.get(position);
-        holder.mTitle.setText(data.name);
+        holder.mTitle.setText(data.operation);
         holder.mSwitch.setChecked(data.isBound);
         ArrayAdapter adapter = new ArrayAdapter<String>(mContext, R.layout.spinner_item, mControls);
         holder.mType.setAdapter(adapter);
         if (data.isBound) {
-            holder.mType.setSelection(data.type);
+            holder.mType.setSelection(data.gesture);
         }
         holder.mType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                data.type = position;
+                data.gesture = position;
+                NetManager.getInstance().getNetService().updateOperation(data.gesture,data.operation)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Observer<Boolean>() {
+                            @Override
+                            public void onCompleted() {
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                e.printStackTrace();
+                                Toast.makeText(mContext,"更新失败",Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onNext(Boolean success) {
+                                if (success){
+                                    Toast.makeText(mContext,"更新成功",Toast.LENGTH_SHORT).show();
+                                }else {
+                                    Toast.makeText(mContext,"更新失败",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
 
             @Override
@@ -69,6 +98,14 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuHolder> {
     @Override
     public int getItemCount() {
         return mList.size();
+    }
+
+    public void setDatas(List<BindData> datas){
+        mList = datas;
+    }
+
+    public List<BindData> getDatas(){
+        return mList;
     }
 
 
